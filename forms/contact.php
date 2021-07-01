fad17202157ec8f27bfdf2379013143cb761ea8a
Nidhi Singh <nidhi232singh@gmail.com>
2021-07-02 02:57:17 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,35 +1,48 @@
 <?php
- 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'nidhi232singh@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/validate.js' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Replace this with your own email address
+$to = 'nidhi232singh@gmail.com';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+function url(){
+  return sprintf(
+    "%s://%s",
+    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+    $_SERVER['SERVER_NAME']
   );
-  */
+}
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+if($_POST) {
 
-  echo $contact->send();
+   $name = trim(stripslashes($_POST['name']));
+   $email = trim(stripslashes($_POST['email']));
+   $subject = trim(stripslashes($_POST['subject']));
+   $contact_message = trim(stripslashes($_POST['message']));
+
+   
+	if ($subject == '') { $subject = "Contact Form Submission"; }
+
+   // Set Message
+   $message .= "Email from: " . $name . "<br />";
+	 $message .= "Email address: " . $email . "<br />";
+   $message .= "Message: <br />";
+   $message .= nl2br($contact_message);
+   $message .= "<br /> ----- <br /> This email was sent from your site " . url() . " contact form. <br />";
+
+   // Set From: header
+   $from =  $name . " <" . $email . ">";
+
+   // Email Headers
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+   ini_set("sendmail_from", $to); // for windows server
+   $mail = mail($to, $subject, $message, $headers);
+
+	if ($mail) { echo "OK"; }
+   else { echo "Something went wrong. Please try again."; }
+
+}
+
 ?>
